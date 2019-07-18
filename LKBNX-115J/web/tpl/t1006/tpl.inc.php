51c2674e51f21a771a28672ab9632f9781032eae
中原優成 <>
2019-07-18 10:11:29 +0900

    AjaxFile削除

--- LKBNX-115J/web/tpl/t1006/tpl.inc.php
+++
@@ -1,1 +0,0 @@
-<div class="%tpl% aiin-ctar"><div class="aiin-inner"><img class="aiin-icon" src="%url-tpl%img/icon.png" width="20" /><span class="aiin-cy">%cy%</span></div></div>
--- LKBNX-115J/web/tpl/t1032/tpl.inc.php
+++
@@ -1,1 +0,0 @@
-<div class="%tpl% aiin-ctar"><div class="aiin-inner"><img class="aiin-icon" src="%url-tpl%img/icon.png" width="72" /><span class="aiin-cy">%cy%</span></div></div>
